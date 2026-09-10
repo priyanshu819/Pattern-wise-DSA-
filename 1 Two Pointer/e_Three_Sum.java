86a6d7bdb90c3e66299d4391356a9d1b11823730
Priyanshu Tiwari <priyanshutiwari8318.2@gmail.com>
2026-09-10 17:55:55 +0530

    Delete all lines

--- 1 Two Pointer/e_Three_Sum.java
+++ 1 Two Pointer/e_Three_Sum.java
@@ -1,8 +1,6 @@
 public class e_Three_Sum {
     public static void main(String[] Args){
-        System.out.println("Hello World");
-        System.out.println("Priyanshu Tiwari");
-
         
+    
     }
 }
