5d5a59a337ad99c994f3df4fe242692f20437a81
Priyanshu Tiwari <priyanshutiwari8318.2@gmail.com>
2026-09-23 00:47:40 +0530

    Add Commit

---
+++ 4_Kedden's Algorithim/c_min_sum_subba.java
@@ -0,0 +1,23 @@
+public class c_min_sum_subba {
+    public static void main(String[] args) {
+        int arr[]={5,4,-1,7,8};
+        c_min_sum_subba a=new c_min_sum_subba();  
+        int ans=a.minSubarraySum(arr);
+        System.out.println("o/p: "+ans);
+    }
+    public int minSubarraySum(int[] arr) {
+        // code here
+        int bestans=arr[0];
+        int ans=arr[0];
+        
+        for(int i=1; i<arr.length; i++){
+            int v1=bestans+arr[i];
+            int v2=arr[i];
+            
+            bestans=Math.min(v1,v2);
+            ans=Math.min(ans,bestans);
+            
+        }
+        return ans;
+    }
+}
