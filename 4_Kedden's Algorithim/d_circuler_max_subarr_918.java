de6e9a2128d3bbd3b4475bc56b6f3816c64c9a16
Priyanshu Tiwari <priyanshutiwari8318.2@gmail.com>
2026-09-25 03:57:36 +0530

    Add commit

--- 4_Kedden's Algorithim/d_circuler_max_subarr_918.java
+++ 4_Kedden's Algorithim/d_circuler_max_subarr_918.java
@@ -1,10 +1,13 @@
 public class d_circuler_max_subarr_918 {
     public static void main(String[] args) {
-        //int a[]={1,-2,3,-2};
-        // int a[]={5,-3,5};
-        // int a[]={-3,-2,-3};
+       //int a[]={1,-2,3,-2};
+        //int a[]={5,-3,5};
+         int a[]={-3,-2,-3};
         //int a[]={3,-2,5};
-        int a[]={2,-2,2,7,8,0};
+        //int a[]={2,-2,2,7,8,0};
+        //
+        
+        //int a[]={6,9,-3};
 
         int ans=maxSubarraySumCircular(a);
         System.out.println("o/p: "+ans);
@@ -13,30 +16,28 @@ public class d_circuler_max_subarr_918 {
 
 
     // i/p: 3,-2,5  ,o/p: 8
+    // i/p: 2,-2,2,7,8,0, o/p:19
+    //i/p:[6,9,-3], o/p:15
     static int maxSubarraySumCircular(int[] nums){
-        int best=nums[0];
-        int ans=nums[0];
-        int n=nums.length;
-        int i=1;
-         while (i<n){
-            int v1=best+nums[i];
-            int v2=nums[i];
-            best=Math.max(v1,v2);
-            ans=Math.max(ans,best);
-            i++;
-           }
-            int ans2=nums[n-1];
-            best=nums[n-1];
-            i=0;
-            while(i<n-1){
-                int v1=best+nums[i];
-                int v2=nums[i];
-                best=Math.max(v1,v2);
-                ans2=Math.max(ans2,best);
-                i++;
-            }
-            ans=Math.max(ans,ans2);
+        int best=nums[0],ans=nums[0];
+        int maxbest=nums[0], maxans=nums[0];
+        int total_Sum=nums[0];
+        for(int i=1; i<nums.length; i++){
+            // For Count max
+            maxbest=Math.max(nums[i],nums[i]+maxbest);
+            maxans=Math.max(maxans,maxbest);
+
+            // For min Count
+            best=Math.min(nums[i],best+nums[i]);
+            ans=Math.min(ans,best);
+
+            total_Sum=total_Sum+nums[i];
+
+        }
+        if (maxans<0)
+            return maxans;
+
+        ans=Math.max(maxans,total_Sum-ans);
         return ans;
     }
-    
 }
